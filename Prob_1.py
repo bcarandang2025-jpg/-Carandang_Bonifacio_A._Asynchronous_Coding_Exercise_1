9b6f9ce593b5f9d7d0f0b150a29b71e2ee9d6715
bcarandang2025-jpg <bcarandang2025@student.nbscollege.edu.ph>
2026-08-27 16:02:48 +0800

    Initial

---
+++ Prob_1.py
@@ -0,0 +1,8 @@
+intro = "Hello, World!"
+numbering = "3,456,344"
+
+new_intro = intro.replace(",", ".")
+new_numbering = numbering.replace(",", ".")
+
+print(new_intro)
+print(new_numbering)
---
+++ Prob_2.py
@@ -0,0 +1,19 @@
+# Problem 2: Filter Characters at Even Indices
+
+foodie = "pizza"
+code = "coding"
+best_friends = "Python"
+letter = "A"
+quotation = " "" "
+
+result = foodie[1::2]
+result2 = code[1::2]
+result3 = best_friends[1::2]
+result4 = letter[0::1]
+result5 = quotation[1::2]
+
+print(result)
+print(result2)
+print(result3)
+print(result4)
+print(result5)
